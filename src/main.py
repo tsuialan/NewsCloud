443d93ebb3358805e1807ab2ed3fb0fb92228161
Alan Tsui <alantsui18@gmail.com>
2020-06-29 20:42:47 -0700

    started webscraping script

--- src/main.py
+++ src/main.py
@@ -12,4 +12,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+     app.run(host="0.0.0.0")
--- src/scripts.py
+++ src/scripts.py
@@ -4,9 +4,22 @@
 #       and scripts needed for newscloud
 
 from bs4 import BeautifulSoup
+import requests
 
 def webscrape():
     print("hello, world!")
+    r1 = requests.get('https://www.nytimes.com/')
+    nyt = r1.content
+
+    bs1 = BeautifulSoup(nyt, 'lxml')
+    nyt = bs1.find_all('h2')
+
+    nyt_hl = []
+    f = open( "nytheadlines.txt", "w")
+    
+    for headline in nyt:
+        nyt_hl.append(headline.getText())
+        f.write(headline.getText() + '\n')
 
 if __name__ == '__main__':
     webscrape()
