0d071cd9d500960a075c6d8e04a4396c68c76f60
Alan Tsui <alantsui18@gmail.com>
2020-07-02 19:15:24 -0700

    push

--- src/newscloud.py
+++ src/newscloud.py
@@ -27,12 +27,32 @@ def newscrape():
 
     nyt = News('New York Times', nyt_hl)
 
-    print(nyt.paper)
-    print(nyt.headlines)
+    #print(nyt.paper)
+    #print(nyt.headlines)
 
-def newsAnalysis():
+    newsAnalysis(nyt)
+
+def newsAnalysis(news):
     print("Hello World")
+
+    wordbank = {}
+    for headline in news.headlines:
+        print(headline)
+        words = headline.split()
+        for word in words:
+            word = word.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+            word = word.replace("'", "").replace('"', "").replace("’", "").replace("‘", "")
+            word = word.lower()
+            print(word)
+            if word not in wordbank.keys():
+                wordbank[word] = 1
+            else:
+                wordbank[word] += 1
+    #print(word_bank)
+
+    f = open("nytheadlines.txt", "a")
+    for key, value in wordbank.items():
+        f.write(key + " : " + str(value) + '\n')
 
 if __name__ == '__main__':
     newscrape()
-    newsAnalysis()
