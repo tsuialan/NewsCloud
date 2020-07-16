42d9279fc9bef9659f27ec97a9be2e076197c221
Gary Kwong <gakwong1@berkeley.edu>
2020-07-15 17:05:48 -0700

    host

--- src/main-flask.py
+++ src/main-flask.py
@@ -13,9 +13,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-<<<<<<< HEAD:src/main.py
      app.run(host='127.0.0.1', port=4000, debug=True)
-=======
-    #app.run(host="0.0.0.0")
-    app.run()
->>>>>>> 7d9d485d20ec516ef4cbfa946a42165eec28febd:src/main-flask.py
