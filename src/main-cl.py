8f5bcb6ad380257aee717df07ef0660fedc4f2e9
Alan Tsui <alantsui18@gmail.com>
2020-07-16 02:31:15 -0700

    needs to refine tags for both scraping functions

--- src/main-cl.py
+++ src/main-cl.py
@@ -15,7 +15,7 @@ def main():
     sindex = "0"
     while (1):
         # "home menu" formatting
-        print("[*] NewsCloud | Home")
+        print("[*] +++ NewsCloud +++")
         counter = 1
         for news in newslist:
             print("[*] " + str(counter) + ": " + news.paper)
@@ -24,8 +24,10 @@ def main():
         sindex = input(
             "[*] Please enter a number corresponding to the paper \n")
         # q to quit program
-        if (sindex.lower() == "q"):
+        if (sindex == "q"):
             break
+        if (sindex == "Q"):
+            return 0
         # calculates index
         sindex = int(sindex) - 1
         # prints out existing newspaper
@@ -39,8 +41,10 @@ def main():
             # formatting for list of keywords
             kindex = input(
                 "[*] Please enter a number corresponding to the keyword \n")
-            if (kindex.lower() == "q"):
+            if (kindex == "q"):
                 break
+            if (kindex == "Q"):
+                return 0
             # calculates index
             kindex = int(kindex) - 1
             kcounter = 1
@@ -54,8 +58,10 @@ def main():
                 # formatting for list of headlines
                 uindex = input(
                     "[*] Please enter a number corresponding to the headline \n")
-                if (uindex.lower() == "q"):
+                if (uindex == "q"):
                     break
+                if (uindex == "Q"):
+                    return 0
                 uindex = int(uindex) - 1
                 url = keyword.urls[uindex]
                 # open selected headline in broweser
