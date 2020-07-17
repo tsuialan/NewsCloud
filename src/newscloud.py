ae05d4721e55da92960c17083e9e3290467677e8
Andy Chen <42586375+anchen31@users.noreply.github.com>
2020-07-16 17:37:41 -0700

    Update newscloud.py

--- src/newscloud.py
+++ src/newscloud.py
@@ -3,13 +3,18 @@
 # Description: creates a word cloud from given
 #   news headlines and keywords
 
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+#from wordcloud import WordCloud
+import newscrape as ns
+import pandas as pd
 
-
-def main():
-    print("hello world!")
-
+#set font size based on frequency
+def setFont():
+    News = ns.main()
+    print(News[0].wordbank)
+    numWords = 0
+    for words in News[0].wordbank:
+        numWords += 1 
+    print(numWords)
 
 if __name__ == '__main__':
-    main()
+    setFont()
